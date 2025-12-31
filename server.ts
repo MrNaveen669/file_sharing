e7c5c06a5015e92c400492f6333f108b76c371a9
unknown <1320204008@bitraipur.ac.in>
2025-12-31 11:28:04 +0530

    Little changes++

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const { initSocketServer } = require('./src/lib/socket');
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
+import { initSocketServer } from './src/lib/socket';
+import './src/lib/cleanup';
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const hostname = '0.0.0.0'; // IMPORTANT for Render
+const port = Number(process.env.PORT) || 3000;
 
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
@@ -13,7 +14,7 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = createServer(async (req, res) => {
     try {
-      const parsedUrl = parse(req.url, true);
+      const parsedUrl = parse(req.url!, true);
       await handle(req, res, parsedUrl);
     } catch (err) {
       console.error('Error occurred handling', req.url, err);
@@ -23,8 +24,6 @@ app.prepare().then(() => {
   });
 
   initSocketServer(server);
-
-    require('./src/lib/cleanup');
 
   server.listen(port, () => {
     console.log(`> Ready on http://${hostname}:${port}`);
